a21d8dd835cf51ed202c7543284b78d69c27f75f
Alexandra Marin <alexandra.marin01@gmail.com>
2018-09-11 19:59:03 +0300

    WIP: add routes and more beacons

--- app/components/beacons/tab-beacons.js
+++ app/components/beacons/tab-beacons.js
@@ -3,14 +3,14 @@ import { observable } from 'mobx';
 import Beacon from './beacon';
 import { tx } from '../utils/translator';
 import uiState from '../layout/ui-state';
-import routerMain from '../routes/router-main';
+import routes from '../routes/routes';
 
 class TabBeacons {
     @observable positionMap = observable.map();
 
     get chatBeacon() {
         const id = 'mobile-chat-tab';
-        const condition = () => routerMain.route === 'chats' && !uiState.isFirstLogin;
+        const condition = () => routes.main.route === 'chats' && !uiState.isFirstLogin;
         const component = (
             <Beacon
                 id={id}
@@ -24,7 +24,7 @@ class TabBeacons {
 
     get fileBeacon() {
         const id = 'mobile-file-tab';
-        const condition = () => routerMain.route.toLowerCase().includes('file') && !uiState.isFirstLogin;
+        const condition = () => routes.main.route.toLowerCase().includes('file') && !uiState.isFirstLogin;
         const component = (
             <Beacon
                 id={id}
@@ -38,13 +38,41 @@ class TabBeacons {
 
     get contactBeacon() {
         const id = 'mobile-contact-tab';
-        const condition = () => routerMain.route.toLowerCase().includes('contact') && !uiState.isFirstLogin;
+        const condition = () => routes.main.route.toLowerCase().includes('contact') && !uiState.isFirstLogin;
         const component = (
             <Beacon
                 id={id}
                 beaconPosition={() => this.positionMap.get(id)}
                 textHeader={tx('title_contact_beacon')}
                 textLine1={tx('description_contact_beacon')}
+            />
+        );
+        return { id, component, condition };
+    }
+
+    get startChatBeacon() {
+        const id = 'mobile-start-dm';
+        const condition = () => true;
+        const component = (
+            <Beacon
+                id={id}
+                beaconPosition={() => this.positionMap.get(id)}
+                textHeader={tx('title_contact_beacon')}
+                textLine1={tx('description_contact_beacon')}
+            />
+        );
+        return { id, component, condition };
+    }
+
+    get uploadFileBeacon() {
+        const id = 'mobile-upload-file';
+        const condition = () => true;
+        const component = (
+            <Beacon
+                id={id}
+                beaconPosition={() => this.positionMap.get(id)}
+                textHeader={tx('title_files_beacon')}
+                textLine1={tx('description_files_beacon')}
             />
         );
         return { id, component, condition };
