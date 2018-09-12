407e61bb9d51cb4bc21e7949e5b294bb7551c5fb
Alexandra Marin <alexandra.marin01@gmail.com>
2018-09-12 14:46:08 +0300

    fix: add beacon back

--- app/components/layout/header-icon-base.js
+++ app/components/layout/header-icon-base.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { observer } from 'mobx-react/native';
 import { View, TouchableOpacity } from 'react-native';
 import SafeComponent from '../shared/safe-component';
-import icons from '../helpers/icons';
 import { vars } from '../../styles/styles';
 import testLabel from '../helpers/test-label';
 import MeasureableIcon from './measureable-icon';
@@ -20,6 +19,7 @@ export default class HeaderIconBase extends SafeComponent {
     icon = '';
     action = () => { };
     disabled = false;
+    beacon = null;
 
     renderThrow() {
         return (
@@ -30,7 +30,10 @@ export default class HeaderIconBase extends SafeComponent {
                     activeOpacity={this.disabled ? 0.5 : 1}
                     {...testLabel(this.props.testID)} >
                     <View style={[itemStyle, this.innerStyle]} >
-                        <MeasureableIcon icon={this.icon} color={vars.white} />
+                        <MeasureableIcon
+                            icon={this.icon}
+                            color={vars.white}
+                            beacon={this.beacon} />
                     </View>
                 </TouchableOpacity>
             </View>
