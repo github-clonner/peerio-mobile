6bd2c3790f80227c9266fa3a1592cc61be967914
Alexandra Marin <alexandra.marin01@gmail.com>
2018-09-13 19:56:12 +0300

    fix: clear state

--- app/components/settings/settings-level-1.js
+++ app/components/settings/settings-level-1.js
@@ -272,7 +272,7 @@ export default class SettingsLevel1 extends SafeComponent {
                     {__DEV__ && <BasicSettingsItem title="reset external setting" onPress={this.resetExternalSetting} />}
                     {__DEV__ && <BasicSettingsItem title="log MC props" onPress={this.showProps} />}
                     {__DEV__ && <BasicSettingsItem title="show saved beacons" onPress={this.showBeaconsState} />}
-                    {__DEV__ && <BasicSettingsItem title="clear saved beacons" onPress={this.showBeaconsState} />}
+                    {__DEV__ && <BasicSettingsItem title="clear saved beacons" onPress={this.clearBeaconsState} />}
                     {/* <BasicSettingsItem title={t('payments')} onPress={() => settingsState.transition('payments')} /> */}
                     {/* <BasicSettingsItem title={t('quotas')} onPress={() => settingsState.transition('quotas')} /> */}
                 </View>
