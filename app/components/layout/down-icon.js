e08c62e8394f696bc356e9e64dc9a904d0c1e556
Samvel Avanesov <seavan@gmail.com>
2018-11-07 09:58:56 -0500

    chore: remove unused components [ch16726]

--- app/components/layout/down-icon.js
+++
@@ -1,6 +0,0 @@
-import HeaderIconBase from './header-icon-base';
-
-export default class DownIcon extends HeaderIconBase {
-    icon = 'arrow-drop-down';
-    action = () => this.props.action && this.props.action();
-}
