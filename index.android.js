675d6700246004c98d04062bcf8b96b6a78fad33
Samvel Avanesov <seavan@gmail.com>
2018-11-16 09:13:56 -0500

    fix: disable animations for android api 24 (nougat) [ch17182]

--- index.android.js
+++ index.android.js
@@ -1,11 +1,17 @@
-import {
-    AppRegistry, UIManager
-} from 'react-native';
+import { AppRegistry, UIManager, Platform } from 'react-native';
 import './shim';
 import App from './app/components/App';
 
 global.platform = 'android';
 
-UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
+function enableAndroidAnimation() {
+    // skip for Android 7.0 (API 24)
+    if (Platform.Version === 24) return;
+    // skip if we don't have the native capability
+    if (!UIManager.setLayoutAnimationEnabledExperimental) return;
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+}
+
+enableAndroidAnimation();
 
 AppRegistry.registerComponent('peeriomobile', () => App);
