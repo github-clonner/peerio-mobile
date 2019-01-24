7d145a0fb767792dbe959added1ed5ebb3b7ea76
Samvel Avanesov <seavan@gmail.com>
2019-01-24 14:35:34 -0500

    chore: make dismissing beacons more failproof [ch246]

--- app/components/beacons/beacon-state.js
+++ app/components/beacons/beacon-state.js
@@ -6,9 +6,11 @@ const notSeen = id => !User.current.beacons.get(id);
 
 class BeaconState {
     @observable beacons = [];
+    @observable disabled = false;
 
     @computed
     get activeBeacon() {
+        if (this.disabled) return null;
         return _.chain(this.beacons)
             .filter(b => notSeen(b.id))
             .filter(b => b.condition())
@@ -34,11 +36,12 @@ class BeaconState {
         User.current.saveBeacons();
     };
 
-    // used for test purposes
-    @action.bound
-    dismissAll() {
-        this.beacons.clear();
-    }
+    // used for test purposes to disable beacons
+    dismissAll = async () => {
+        this.disabled = true;
+        // let beacons disappear
+        await new Promise(resolve => setTimeout(resolve, 500));
+    };
 }
 
 const beaconState = new BeaconState();
